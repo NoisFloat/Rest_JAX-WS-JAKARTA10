4922c12f8cf3fd1c28b85530523de850d82d2728
Nois2 <103067863+Nois2@users.noreply.github.com>
2024-07-18 22:34:28 -0600

    Antes de dependencias

---
+++ src/main/java/sv/edu/udb/rest_g3udbgf222473/HelloApplication.java
@@ -0,0 +1,9 @@
+package sv.edu.udb.rest_g3udbgf222473;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
